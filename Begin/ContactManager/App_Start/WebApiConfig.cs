a9cf9f5f94fba454222f5c4250811708f7153be4
Domagoj Bunoza <domagoj.bunoza@student.ferit.hr>
2021-11-15 17:00:16 +0100

    Creating the Contact Manager API Controllers

--- Begin/ContactManager/App_Start/WebApiConfig.cs
+++ Begin/ContactManager/App_Start/WebApiConfig.cs
@@ -17,8 +17,14 @@ namespace ContactManager
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
+
                 defaults: new { id = RouteParameter.Optional }
+
             );
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+            config.Formatters.Remove(config.Formatters.FormUrlEncodedFormatter);
         }
     }
 }
